91b41c936d2340eaaf3d712e6d79aeca20d9c067
kguinto <klogold1@gmail.com>
2018-03-09 22:48:16 -0800

    Moved functions to separate files and changed routes for data and bundle.js

--- server/server.js
+++ server/server.js
@@ -1,51 +1,18 @@
 const express = require('express');
-const { Restaurant, Hotel, Attraction } = require('../models/models.js');
 const cors = require('cors');
+const { getOneRestaurant, getOneHotel, getOneAttraction } = require('./dbFunctions');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.use(cors());
 
-app.use('/bundle.js', express.static('public/build/bundle.js'));
+app.use('/:id', express.static('public'));
+app.use('/nearby/bundle.js', express.static('public/build/bundle.js'));
 
-app.use('/:id', express.static('public/'));
-
-app.get('/restaurants/:id', (req, res) => {
-  Restaurant.findOne({ where: { restaurant_id: req.params.id } })
-    .then(restaurant => restaurant.dataValues)
-    .then((restaurant) => {
-      res.send(restaurant);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send();
-    });
-});
-
-app.get('/hotels/:id', (req, res) => {
-  Hotel.findOne({ where: { hotel_id: req.params.id } })
-    .then(hotel => hotel.dataValues)
-    .then((hotel) => {
-      res.send(hotel);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send();
-    });
-});
-
-app.get('/attractions/:id', (req, res) => {
-  Attraction.findOne({ where: { attraction_id: req.params.id } })
-    .then(attraction => attraction.dataValues)
-    .then((attraction) => {
-      res.send(attraction);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send();
-    });
-});
+app.get('/nearby/restaurants/:id', getOneRestaurant);
+app.get('/nearby/hotels/:id', getOneHotel);
+app.get('/nearby/attractions/:id', getOneAttraction);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
