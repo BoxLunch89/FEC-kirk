f40e768be7f80c9b1035979db86874a84e953350
kguinto <klogold1@gmail.com>
2018-03-10 09:33:43 -0800

    Even more info in error logs

--- client/dataHelpers.js
+++ client/dataHelpers.js
@@ -6,11 +6,24 @@ const getItem = (type, id) => (
       .then((item) => {
         resolve(item.data);
       })
-      .catch((err) => {
+      .catch((error) => {
         console.log('Error getting item', type, id);
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
       });
   })
 );
